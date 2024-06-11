e337bcede8f3d0954b629247c55e188d05441226
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2024-06-11 17:56:14 +0530

    config files added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+        port: "",
+        pathname: "/coins/images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
