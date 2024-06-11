662dcbc80375183dfa7d7dad442f438df73a5f57
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2024-06-11 17:48:02 +0530

    added the loading page for server side rendered pages

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="dots-loader">
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+    </div>
+  );
+};
+
+export default Loader;
